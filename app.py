5479d9464fabc841bf276842d9d7f575ddeef0f2
Petar Petrov <ppp1808@gmail.com>
2026-02-04 15:28:05 +0200

    In navigation hyper link for 'Posts'I added the letter 's' In home.html in recent-posts section It will only appear post title without the content In post.html I added filter of post content so if it has HTML tags to not escape it Created three new python files - run.py, routes.py, extensions.py Run.py is when you want to run the app, routes.py is to be more clear and separate from app.py logic and extensions.py is for more separate and easy using SQLAlchemy In app.py made a function to create the app with database (PostgreSQL), so when i need to import the app to be more flexible and easy to find. In static foulder, added style and images foulders

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False     
-
-db = SQLAlchemy(app)
-
-from models import Post
-
-@app.route("/")
-def home():
-    name = "Peter"
-    return render_template('home.html', name=name)
+from extensions import db
+from routes import register_app
+from decouple import config
 
 
-@app.route("/about")
-def about():
-    name = "Peter"
-    framework = "Flask"
-    return render_template('about.html', name=name, framework=framework)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/posts")
-def post():
-    posts = Post.query.all()
-    return render_template('post.html', posts=posts)
+    DB_USER = config('DB_USER')
+    DB_PASSWORD = config('DB_PASSWORD')
+    DB_NAME = config('DB_NAME')
+    DB_HOST = config('DB_HOST', default='localhost')
+    DB_PORT = config('DB_PORT', default=5432)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
+    db.init_app(app)
+
+    register_app(app)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
--- create_db.py
+++ create_db.py
@@ -1,6 +1,8 @@
-from app import app, db
-from models import Post
+from app import create_app
+from extensions import db
+
+app = create_app()
 
 with app.app_context():
     db.create_all()
-    print("Database and tables created successufully!")
+    print("Database created!")
